6b3946609edd495215801144d6476a8ac8277451
Marcos Colella <marcos.colella@gmail.com>
2024-01-12 15:07:48 -0300

    Configuramos o Contexto a autenticação e o Login salvo no LocalStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,53 @@
-import {createContext, useContext} from "react"
+import {createContext, useContext, useState, useEffect} from "react"
+
+import { api } from "../services/api" //Importamos o banco
 
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
+    const [data, setData] = useState({}) //Para armazenar as infos do Usuario e Token
+
+    async function signIn({ email, password}) {
+
+        try {
+            const response = await api.post("/sessions", { email, password }) //Criamos a requisição
+            const { user, token} = response.data // Desestruturar p/ pegar apenas as informações do usuário
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+            localStorage.setItem("@rocketnotes:token", token)
+
+            api.defaults.headers.authorization = `Bearer ${token}` //aplicando o Token em todas as requisições q o usuário for fazer
+            setData({ user, token}) // Amazenamos o usuario e token no data
+
+             console.log(user, token)
+
+        } catch (error) {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possível entrar.")
+            }
+        }
+        
+    }
+
+    useEffect(() => {
+        const user = localStorage.getItem("@rocketnotes:user")
+        const token = localStorage.getItem("@rocketnotes:token")
+
+        if (token && user ) {
+            api.defaults.headers.authorization = `Bearer ${token}`
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, [])
+
+    // o user: data.user  esta armazenando o usuário logado e passando por contexto p/ aplicação
     return (
-        <AuthContext.Provider value={{email:"marcos@gmail.com"}}>
+        <AuthContext.Provider value={{signIn, user: data.user}}> 
             { children }
         </AuthContext.Provider>
     )
