11a39aba3d7bf0d0ee1a5713edcd640757793b66
Marcos Colella <marcos.colella@gmail.com>
2024-01-15 14:18:32 -0300

    função signOut

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -31,6 +31,13 @@ function AuthProvider({ children }) {
         
     }
 
+    function signOut() {
+        localStorage.removeItem("@rocketnotes:user")
+        localStorage.removeItem("@rocketnotes:token")
+
+        setData({})
+    }
+
     useEffect(() => {
         const user = localStorage.getItem("@rocketnotes:user")
         const token = localStorage.getItem("@rocketnotes:token")
@@ -47,7 +54,7 @@ function AuthProvider({ children }) {
 
     // o user: data.user  esta armazenando o usuário logado e passando por contexto p/ aplicação
     return (
-        <AuthContext.Provider value={{signIn, user: data.user}}> 
+        <AuthContext.Provider value={{signIn, signOut, user: data.user}}> 
             { children }
         </AuthContext.Provider>
     )
