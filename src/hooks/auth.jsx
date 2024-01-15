cb1c1673566a9d45d9f95059d888ee2adca0d623
Marcos Colella <marcos.colella@gmail.com>
2024-01-15 15:18:21 -0300

    Atualizando Profile Usuário

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,7 +16,7 @@ function AuthProvider({ children }) {
             localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
             localStorage.setItem("@rocketnotes:token", token)
 
-            api.defaults.headers.authorization = `Bearer ${token}` //aplicando o Token em todas as requisições q o usuário for fazer
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}` //aplicando o Token em todas as requisições q o usuário for fazer
             setData({ user, token}) // Amazenamos o usuario e token no data
 
              console.log(user, token)
@@ -38,12 +38,30 @@ function AuthProvider({ children }) {
         setData({})
     }
 
+    async function updateProfile( { user }) {
+        try {
+
+            await api.put("/users", user)
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+
+            setData({user, token: data.token})
+            alert("Perfil atualizado!")
+
+        } catch (error) {
+            if(error.response) {
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possível atualizar.")
+            }
+        }
+    }
+
     useEffect(() => {
         const user = localStorage.getItem("@rocketnotes:user")
         const token = localStorage.getItem("@rocketnotes:token")
 
         if (token && user ) {
-            api.defaults.headers.authorization = `Bearer ${token}`
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
 
             setData({
                 token,
@@ -54,7 +72,7 @@ function AuthProvider({ children }) {
 
     // o user: data.user  esta armazenando o usuário logado e passando por contexto p/ aplicação
     return (
-        <AuthContext.Provider value={{signIn, signOut, user: data.user}}> 
+        <AuthContext.Provider value={{signIn, signOut, updateProfile, user: data.user}}> 
             { children }
         </AuthContext.Provider>
     )
