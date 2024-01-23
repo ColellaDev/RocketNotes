7b3dcc2627ac5ebdfc0a6186b20db118d8a4c61e
Marcos Colella <colella.dev@gmail.com>
2024-01-23 12:10:33 -0300

    Carregamos as Tags no Menu para funcionarem como filtros futuramente

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,8 @@
+import  { useState, useEffect } from "react"
 import { FiPlus, FiSearch } from 'react-icons/fi'
+
+import { api } from "../../services/api"
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
@@ -8,6 +12,32 @@ import { Note } from '../../components/Note'
 import { Section } from '../../components/Section'
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName){
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+      
+    } else {
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+
+    
+  }
+
+  useEffect(() => {
+    async function fetchTags() {
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+  }, [])
+
   return (
     <Container>
       <Brand>
@@ -17,9 +47,26 @@ export function Home() {
       <Header></Header>
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+      <li>
+        <ButtonText 
+        title="Todos" 
+        onClick={() => handleTagSelected("all")}
+        isActive={tagsSelected.length === 0}
+         />
+     </li>
+        {
+            tags && tags.map(tag => (
+              <li key={String(tag.id)}>
+                   <ButtonText 
+                   title={tag.name}
+                   onClick={() => handleTagSelected(tag.name)}
+                   isActive={tagsSelected.includes(tag.name)} //Retorna verdadeiro caso a tag.name exista la dentro
+                   />
+              </li>
+            ))
+           
+        }
+       
       </Menu>
 
       <Search>
